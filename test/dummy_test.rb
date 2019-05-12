178a134579f1b138275070f81684cbd3750b89ba
Mads Storm Hansen <madsstorm@gmail.com>
2019-05-12 19:08:16 +0200

    Update dummy_test.rb

--- test/dummy_test.rb
+++ test/dummy_test.rb
@@ -1,7 +1,23 @@
+# nested test case example.
 require 'test/unit'
 
-class Dummy_Test < Test::Unit::TestCase
-    def numbers_equal
-      assert_equal(2, 3)
+class UserTest < Test::Unit::TestCase
+  def setup
+    @user = "me"
+  end
+
+  def test_full_name
+    assert_equal("me", @user)
+  end
+
+  class ProfileTest < UserTest
+    setup
+    def setup_profile
+      @user += ": profile"
+    end
+
+    def test_has_profile
+      assert_match(/: profile/, @user)
     end
   end
+end
